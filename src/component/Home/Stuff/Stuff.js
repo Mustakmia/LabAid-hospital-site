0534b18e4724d685574260916952d12d6ba7c291
Mustakmia <musyafijur52@gmail.com>
2021-10-19 23:28:33 +0600

    added

--- src/component/Home/Stuff/Stuff.js
+++ src/component/Home/Stuff/Stuff.js
@@ -16,12 +16,7 @@ const Stuff = () => {
                     </div>
                     <div className="col-12 col-md-7">
                         <div className="stuffGrid mx-auto w-100">
-                            <div style={{ height: '120px', width: '120px', border: '2px solid tomato' }}><img className="w-100 mx-auto h-100" src="https://image.freepik.com/free-photo/smiling-asian-woman-physician-white-coat_9083-2472.jpg" alt="" /></div>
-                            <div style={{ height: '120px', width: '120px', border: '2px solid tomato' }}><img className="w-100 mx-auto h-100" src="https://image.freepik.com/free-photo/smiling-young-female-doctor-wearing-medical-robe-with-stethoscope-spreading-hand-isolated-green-wall_141793-86098.jpg" alt="" /></div>
-                            <div style={{ height: '120px', width: '120px', border: '2px solid tomato' }}><img className="w-100 mx-auto h-100" src="https://image.freepik.com/free-photo/successful-medical-team_329181-9252.jpg" alt="" /></div>
-                            <div style={{ height: '120px', width: '120px', border: '2px solid tomato' }}><img className="w-100 mx-auto h-100" src="https://image.freepik.com/free-photo/hospital-healthcare-workers-covid-19-treatment-concept-young-doctor-scrubs-making-daily-errands-clinic-listening-patient-symptoms-look-camera-professional-physician-curing-diseases_1258-57233.jpg" alt="" /></div>
-                            <div style={{ height: '120px', width: '120px', border: '2px solid tomato' }}><img className="w-100 mx-auto h-100" src="https://image.freepik.com/free-photo/portrait-smiling-male-doctor_171337-1532.jpg" alt="" /></div>
-                            <div style={{ height: '120px', width: '120px', border: '2px solid tomato' }}><img className="w-100 mx-auto h-100" src="https://image.freepik.com/free-photo/pleased-young-female-doctor-wearing-medical-robe-stethoscope-around-neck-standing-with-closed-posture_409827-254.jpg" alt="" /></div>
+                            <div style={{ height: '100%', width: '100%', border: '2px solid tomato' }}><img className="w-100 mx-auto h-100" src="https://image.freepik.com/free-photo/smiling-asian-woman-physician-white-coat_9083-2472.jpg" alt="" /></div>
                         </div>
                     </div>
                 </div>
